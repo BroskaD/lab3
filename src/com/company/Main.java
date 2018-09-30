25cffd288c68016193fd4a58415f0110a6716335
BroskaD <GITHUB dmitrij.broska98@gmail.com>
2018-09-30 22:56:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,112 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        byte section = 0;
+
+        while (true) {
+            System.out.println("1.Проверка теоремы Ферма");
+            System.out.println("2.Вычисление выражения 1-1/2+1/3-1/4+...+1/9999-1/10000");
+            section = scan.nextByte();
+            switch (section) {
+
+                case 1:
+                    Ferma();
+                    break;
+                case 2:
+                    Row();
+                    break;
+                default:
+                    System.out.println("Неверно указанное действие.");
+            }
+        }
+    }
+
+    public static void Ferma() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Проверка теоремы Ферма.\n" +
+                "Введите степень n>2, чтобы проверить существуют ли три числа в диапазоне от 1 до 100,\n" +
+                " соответствующие условию а^+b^n=c^n");
+        long a = 0;
+        long b = 0;
+        long c= 0;
+        long n = 0;
+        int limit = 101;
+        double result = 0;
+        boolean flag = false;
+        System.out.println("Задайте степень n :");
+        while (true) {
+            n = scan.nextInt();
+            if (n > 2)
+                break;
+            System.out.println("Значение степени должно быть более двух!");
+        }
+
+        for (a = 1; a < limit; a++) {
+            for (b = 1; b < limit; b++) {
+                for (c = 1; c < limit; c++) {
+                    result = Math.pow((double) a, (double) n) +
+                            Math.pow((double) b, (double) n);
+                    if (result == Math.pow((double) c, (double) n)) {
+                        flag = true;
+                        System.out.println("Решение найдено!\na=" + a +
+                                " b=" + b + " c=" + c);
+                    }
+
+                }
+            }
+        }
+        if (flag == false)
+            System.out.println("Для заданных чисел и степени решения не найдено.");
+
+        int section = 0;
+        System.out.println("Проверить решения для n=2?\n" +
+                "(нажмите 1 )");
+        section = scan.nextInt();
+        switch (section) {
+            case 1:
+                n = 2;
+                for (a = 1; a < limit; a++) {
+                    for (b = 1; b < limit; b++) {
+                        for (c = 1; c < limit; c++) {
+                            result = Math.pow((double) a, (double) n) +
+                                    Math.pow((double) b, (double) n);
+                            if (result == Math.pow((double) c, (double) n)) {
+                                flag = true;
+                                System.out.println("Решение найдено!\na=" + a +
+                                        " b=" + b + " c=" + c);
+                            }
+
+                        }
+                    }
+                }
+
+
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public static void Row() {
+        double result = 0;
+
+        System.out.println("Вычисление значения выражения 1-1/2+1/3-1/4+...+1/9999-1/10000");
+        for (int i = 1; i <= 10000; i++) {
+            if (i % 2 == 0) {
+                result = result - ((1) / (double) i);
+                //System.out.println(result+"\n");
+            } else {
+                result += 1 / (double) i;
+                //System.out.println(result+"\n");
+                System.out.println("1-1/2+1/3-1/4+...+1/9999-1/10000=" + result);
+            }
+        }
+    }
+}
